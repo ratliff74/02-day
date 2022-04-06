2ac3bb8688ef14024e8ff017da80e759c9acc1ae
ratliff74 <101727247+ratliff74@users.noreply.github.com>
2022-04-06 09:38:50 -0500

    Add files via upload

---
+++ module.py
@@ -0,0 +1,8 @@
+from cmath import pi
+
+
+def circumference( pi , radius):
+  result = "2" * pi * radius 
+  print("The circumference of the circle is", result)
+
+circumference(radius= 12)
---
+++ module_2.py
@@ -0,0 +1,9 @@
+
+
+
+def square_footage(length, width, height):
+  result= length * width * height
+  print("The square footage is", result)
+
+input("How to calculate S2:")
+
